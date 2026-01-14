f8935ade772154eca6becf4f90c9ed019d36131a
Виталий Якимов <fess.888@mail.ru>
2026-01-14 21:43:40 +0600

    fixed the error mypy

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-import masks
+from masks import get_mask_account, get_mask_card_number # type: ignore
 
 
 def mask_account_card(account_or_card_number: str) -> str:
@@ -12,10 +12,10 @@ def mask_account_card(account_or_card_number: str) -> str:
             name_card_or_account += letter
     if account_or_card_number.startswith("Счет"):
         account_number = int(account_or_card_number[5:])
-        return name_card_or_account + masks.get_mask_account(account_number)
+        return name_card_or_account + get_mask_account(account_number)
     else:
         card_number = int(account_or_card_number[-16:])
-        return name_card_or_account + masks.get_mask_card_number(card_number)
+        return name_card_or_account + get_mask_card_number(card_number)
 
 
 def get_date(long_date: str) -> str:
